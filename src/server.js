8f4f85dffa721dbe69cca4525c70e9bf0bc375c1
Willian Praciano <willian.s.praciano@gmail.com>
2022-04-11 13:14:08 -0300

    adding proeject files

---
+++ src/server.js
@@ -0,0 +1,128 @@
+const fs = require('fs/promises');
+const path = require('path');
+
+const automl = require('@tensorflow/tfjs-automl');
+const tf = require('@tensorflow/tfjs-node');
+
+const sharp = require('sharp');
+
+const color = require('dominant-color');
+
+const vibrant = require('node-vibrant');
+
+let dominantArray = [];
+let vibrantArray = [];
+let darkVibrantArray = [];
+let lightVibrantArray = [];
+let mutedArray = [];
+let darkMutedArray = [];
+let lightMutedArray = [];
+
+function save(outputFile, object){
+  const outputPath = path.resolve(__dirname, 'output', outputFile);
+  fs.writeFile(outputPath, JSON.stringify(object, '', 2), { flag: 'w' });
+}
+
+function newArray(color, outputName){
+  let arr = {};
+  arr.red = Math.round(Number(color[0]));
+  arr.green = Math.round(Number(color[1]));
+  arr.blue = Math.round(Number(color[2]));
+  arr.file = outputName;
+  arr.label = 'T';
+
+  return arr;
+}
+
+function getColor(outputName) {
+  const imageCropped = path.resolve(__dirname, 'output', outputName);
+  
+  color(imageCropped, { format: 'rgb' }, function (err, color ) {
+    
+    dominantArray.push(newArray(color, outputName));
+
+    save('dominant.json', dominantArray);
+  });
+
+  vibrant.from(imageCropped).getPalette((err, palette) => {
+    // console.log(palette);
+    vibrantArray.push(newArray(palette.Vibrant._rgb, outputName));
+    save('vibrant.json', vibrantArray);
+
+    darkVibrantArray.push(newArray(palette.DarkVibrant._rgb, outputName));
+    save('darkVibrant.json', darkVibrantArray);
+
+    lightVibrantArray.push(newArray(palette.LightVibrant._rgb, outputName));
+    save('lightVibrant.json', lightVibrantArray);
+
+    mutedArray.push(newArray(palette.Muted._rgb, outputName));
+    save('muted.json', mutedArray);
+
+    darkMutedArray.push(newArray(palette.DarkMuted._rgb, outputName));
+    save('darkMuted.json', darkMutedArray);
+
+    lightMutedArray.push(newArray(palette.LightMuted._rgb, outputName));
+    save('lightMuted.json', lightMutedArray);
+
+  });
+}
+
+async function cropImage(image, imageName, width, height, left, top) {
+  console.log(imageName);
+
+  const outputName = `cropped_${imageName}`;
+
+  await sharp(image)
+    .extract({ width, height, left, top })
+    .toFile(path.resolve(__dirname, 'output', outputName))
+    .then(() => console.log(`Image ${imageName} cropped and saved`))
+    .catch((err) => console.log('Error when cropping image'));
+
+  await getColor(outputName);
+}
+
+async function getMucosa(imageFolder, imageName) {
+  const modelJSON = 'file://src/model/model.json';
+  const imagePath = path
+    .resolve(__dirname, imageFolder, imageName);
+
+  const image = await fs.readFile(imagePath);
+  const tensor = tf.node.decodeImage(image);
+
+  const graphModel = await tf.loadGraphModel(modelJSON);
+  const model = new automl
+    .ObjectDetectionModel(graphModel, ['background','Mucosa']);
+    
+  try {
+    const options = { score: 0.2, iou: 0.5, topk: 20 };
+    const predictions = await model.detect(tensor, options);
+    // console.log(predictions);
+    
+    cropImage(
+      image,
+      imageName,
+      Math.round(predictions[0].box.width * 0.5),
+      Math.round(predictions[0].box.height * 0.5),
+      Math.round(predictions[0].box.left + predictions[0].box.width * 0.25),
+      Math.round(predictions[0].box.top + predictions[0].box.height * 0.25),
+    );
+    
+    return predictions;
+  } catch(err) {
+    console.log(err);
+  }
+}
+
+async function readDir(rootDir) {
+  rootDir = rootDir || path.resolve(__dirname);
+
+  const files = await fs.readdir(rootDir);
+  
+  for(let file of files) {
+    await getMucosa('images', file);
+  }
+
+  console.log(dominantArray);
+}
+
+readDir(path.resolve(__dirname, './images'));

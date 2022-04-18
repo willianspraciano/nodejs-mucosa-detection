ec0b26b084cf9a25621977f3f4519e98695b10b0
Willian S. Praciano <willian.s.praciano@gmail.com>
2022-04-18 08:39:46 -0300

    feat: adding treatment when RGB color comes in percentage

--- src/server.js
+++ src/server.js
@@ -18,34 +18,52 @@ let mutedArray = [];
 let darkMutedArray = [];
 let lightMutedArray = [];
 
-function save(outputFile, object){
+function save(outputFile, object) {
   const outputPath = path.resolve(__dirname, 'output', outputFile);
   fs.writeFile(outputPath, JSON.stringify(object, '', 2), { flag: 'w' });
 }
 
-function newArray(color, outputName){
+function newArray(color, outputName) {
+  let colorNumber = [];
+
+  // Se a cor vier em porcentagem
+  if (typeof color[0] === 'string' && color[0].includes('%')) {
+    colorNumber = color.map((el, i) => {
+      el = Number(el.replace('%', ''));
+
+      // converte a porcentagem de para um número de 0 a 255
+      return Math.round((el * 255) / 100);
+    });
+  } else {
+    colorNumber = color;
+  }
+
   let arr = {};
-  arr.red = Math.round(Number(color[0]));
-  arr.green = Math.round(Number(color[1]));
-  arr.blue = Math.round(Number(color[2]));
+  arr.red = Math.round(Number(colorNumber[0]));
+  arr.green = Math.round(Number(colorNumber[1]));
+  arr.blue = Math.round(Number(colorNumber[2]));
   arr.file = outputName;
   arr.label = 'T';
 
   return arr;
 }
 
-function getColor(outputName) {
+async function getColor(outputName) {
   const imageCropped = path.resolve(__dirname, 'output', outputName);
-  
-  color(imageCropped, { format: 'rgb' }, function (err, color ) {
-    
+
+  console.log('image: ', imageCropped);
+  await color(imageCropped, { format: 'rgb' }, function (err, color) {
+    // console.log('erro: ', err);
+    // console.log('Color: ', color);
+    // console.log('Arr: ', newArray(color, outputName));
     dominantArray.push(newArray(color, outputName));
 
     save('dominant.json', dominantArray);
   });
 
   vibrant.from(imageCropped).getPalette((err, palette) => {
-    // console.log(palette);
+    console.log(palette);
+    console.log(err);
     vibrantArray.push(newArray(palette.Vibrant._rgb, outputName));
     save('vibrant.json', vibrantArray);
 
@@ -63,7 +81,6 @@ function getColor(outputName) {
 
     lightMutedArray.push(newArray(palette.LightMuted._rgb, outputName));
     save('lightMuted.json', lightMutedArray);
-
   });
 }
 
@@ -83,32 +100,34 @@ async function cropImage(image, imageName, width, height, left, top) {
 
 async function getMucosa(imageFolder, imageName) {
   const modelJSON = 'file://src/model/model.json';
-  const imagePath = path
-    .resolve(__dirname, imageFolder, imageName);
+  const imagePath = path.resolve(__dirname, imageFolder, imageName);
 
   const image = await fs.readFile(imagePath);
   const tensor = tf.node.decodeImage(image);
 
   const graphModel = await tf.loadGraphModel(modelJSON);
-  const model = new automl
-    .ObjectDetectionModel(graphModel, ['background','Mucosa']);
-    
+  const model = new automl.ObjectDetectionModel(graphModel, [
+    'background',
+    'Mucosa',
+  ]);
+
   try {
     const options = { score: 0.2, iou: 0.5, topk: 20 };
     const predictions = await model.detect(tensor, options);
-    // console.log(predictions);
-    
-    cropImage(
-      image,
-      imageName,
-      Math.round(predictions[0].box.width * 0.5),
-      Math.round(predictions[0].box.height * 0.5),
-      Math.round(predictions[0].box.left + predictions[0].box.width * 0.25),
-      Math.round(predictions[0].box.top + predictions[0].box.height * 0.25),
-    );
-    
+    console.log(predictions);
+
+    predictions.length > 0 &&
+      cropImage(
+        image,
+        imageName,
+        Math.round(predictions[0].box.width * 0.5),
+        Math.round(predictions[0].box.height * 0.5),
+        Math.round(predictions[0].box.left + predictions[0].box.width * 0.25),
+        Math.round(predictions[0].box.top + predictions[0].box.height * 0.25)
+      );
+
     return predictions;
-  } catch(err) {
+  } catch (err) {
     console.log(err);
   }
 }
@@ -117,8 +136,8 @@ async function readDir(rootDir) {
   rootDir = rootDir || path.resolve(__dirname);
 
   const files = await fs.readdir(rootDir);
-  
-  for(let file of files) {
+
+  for (let file of files) {
     await getMucosa('images', file);
   }
 
@@ -126,3 +145,5 @@ async function readDir(rootDir) {
 }
 
 readDir(path.resolve(__dirname, './images'));
+
+// getColor('cropped_19316_SO_19_09-12-2019_iOS_3015_EDITADO.jpg');
